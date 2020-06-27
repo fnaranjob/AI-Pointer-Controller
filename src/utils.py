a1d8d60463e8d12dc7bf8d5581a59175d4906fca
Fernando Naranjo B <fnaranjob@supracnc.com>
2020-06-26 21:10:57 -0500

    fix: Change crop dimensions to square aspect ratio

--- src/utils.py
+++ src/utils.py
@@ -7,28 +7,23 @@ PADDING_COLOR = [0,0,0]
 def crop_image(image, boxes):
 	'''
 	Returns:
-		0 if no face was detected
-		-1 if more than one face was detected
-		cropped face if exactly one face was detected
-	Adds padding to make the processed image square, this prevents distortion when feeding to next model
+		0 if boxes is empty
+		cropped image (forced square aspect ratio) if not
 	'''
 	if len(boxes)==0:
 		return 0
-	elif len(boxes)>1:
-		return -1
 	else:
-		x1=boxes[0]['pt1'][0]
-		x2=boxes[0]['pt2'][0]
-		y1=boxes[0]['pt1'][1]
-		y2=boxes[0]['pt2'][1]
-		crop_img = image[y1:y2, x1:x2]
-	
-	box_height=y2-y1
-	box_width=x2-x1
-	output_side_length = max(box_width, box_height)
-	
-	pad_x=(output_side_length-box_width)//2
-	pad_y=(output_side_length-box_height)//2
+		cropped_images=[]
+		for i,box in enumerate(boxes):
+			x1=box['pt1'][0]
+			x2=box['pt2'][0]
+			y1=box['pt1'][1]
+			y2=box['pt2'][1]
+			box_height=y2-y1
+			box_width=x2-x1
+			output_side_length = max(box_width, box_height)
+			pad_x=(output_side_length-box_width)//2
+			pad_y=(output_side_length-box_height)//2
+			cropped_images.append(image[y1-pad_y:y2+pad_y, x1-pad_x:x2+pad_x])
 
-	processed_img= cv2.copyMakeBorder(crop_img,pad_y,pad_y,pad_x,pad_x,cv2.BORDER_CONSTANT,value=PADDING_COLOR)
-	return processed_img
+	return cropped_images
